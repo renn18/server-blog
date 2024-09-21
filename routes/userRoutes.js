754d22181724d4df3aac5692a13f0517708bf486
Renn <ppskdos@gmail.com>
2024-09-22 05:08:15 +0800

    add some spicy flavor

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ const router = Router()
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
-router.get('/:id', getUser)
+router.get('/get/:id', getUser)
 router.get('/', getAuthors)
 router.post('/change-avatar', authMiddleware, changeAvatar)
 router.patch('/edit-user', authMiddleware, editUser)
